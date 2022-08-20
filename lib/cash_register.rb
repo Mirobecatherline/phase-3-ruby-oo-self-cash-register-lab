3e760502d84453d1044b72a483b1fc043ef8e25a
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-08-20 18:49:50 +0300

    commit tree 86a239269413fd6054b8971a9fff29d25d72381f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+    attr_accessor :total,:discount
+    def initialize (total=0,discount)
+        @total=total
+        @discount=discount
+    end
+    def total
+        @total
+    end
+    def add_item
+        
+    end
+    def apply_discount
+        
+    end
+    def items()
+        
+    end
+    def void_last_transaction
+        @total
+    end
+end
 
